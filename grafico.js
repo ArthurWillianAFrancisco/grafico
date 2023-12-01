3ef9cafb40a5b251d19225a57b712bfd536320d3
Arthur Willian Assunção Francisco <109974170+ArthurWillianAFrancisco@users.noreply.github.com>
2023-12-01 12:32:45 -0400

    Update grafico.js

--- grafico.js
+++ grafico.js
@@ -1,14 +1,23 @@
-//Variavel do genero
+//contador genero
 var contH = 0;
 var contM = 0;
 
-//variavel da faixa etaria
+//contador faixa etaria
 var contbebe = 0;
 var contcrianca = 0;
 var contadolecente = 0;
 var contadulto = 0;
 var contidoso = 0;
-function myFunction() {
+
+//contador cor
+var contbranco = 0;
+var contamarelo = 0;
+var contnegro = 0;
+var contindio = 0;
+
+
+async function myFunction() {
+    //variavel e codigo genero
     var H = document.getElementById("myRadio").checked;
     var M = document.getElementById("myRadio2").checked;
     if(H){
@@ -17,7 +26,9 @@ function myFunction() {
         contM++;
     }  
 
-   
+  
+
+    //variavel e codigo faixa etaria
     var bebe = document.getElementById("bebe").checked;
     var crianca = document.getElementById("crianca").checked;
     var adolecente = document.getElementById("adolecente").checked;
@@ -34,13 +45,30 @@ function myFunction() {
         contadulto++;
     }else if(idoso) {
         contidoso++;
-    } 
+    };
+  
+    //variavel e codigo raça
+    var branco = document.getElementById("branco").selected;
+    var amarelo = document.getElementById("amarelo").selected;
+    var negro = document.getElementById("negro").selected;
+    var indio = document.getElementById("indio").selected;
+	
+    if(branco){
+        contbranco++;
+    }else if(amarelo) {
+        contamarelo++;
+    }else if(negro) {
+        contnegro++;
+    }else if(indio) {
+        contindio++;
+    };
 }
-
 
 function Grafico(){
     
     var sexo = [contH, contM]
+    alert(sexo)
+
     // Seus dados de gráfico aqui (exemplo)
     var data = {
         labels: ["Homens", "Mulheres"],
@@ -57,21 +85,22 @@ function Grafico(){
     };
 
     // Criar gráfico de pizza
+
     var ctx = document.getElementById('pieChart').getContext('2d');
     var pieChart = new Chart(ctx, {
         type: 'pie',
         data: data,
         options: options
     })
-//----------------------------------------------------------------//
-//faixa etaária 
+//------------------------------------------------------------------------//
+//faixa etinia 
 
     var idade = [
-    contbebe, 
-    contcrianca,
-    contadolecente,
-    contadulto,
-    contidoso]
+        contbebe, 
+        contcrianca,
+        contadolecente,
+        contadulto,
+        contidoso]
 
     var data = {
         labels: ["Bebê", "Criança", "Adolecente", "Adulto", "Idoso"],
@@ -94,4 +123,42 @@ function Grafico(){
         data: data,
         options: options
     })
+//----------------------------------------------------------------------//    
+// raça
+
+    var raca = [
+        contbranco, 
+        contamarelo,
+        contnegro,
+        contindio]
+    alert(raca)
+   
+    
+    var data = {
+        labels: ["branco", "amarelo", "negro", "indio"],
+        datasets: [{
+        data: raca,
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
+        }]
+    };
+
+    // Configuração do gráfico
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+    };
+
+    // Criar gráfico de pizza
+    var ctx = document.getElementById('pieChart3').getContext('2d');
+    var pieChart3 = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    })
+}
+function reinicia(){
+    se;
+    myLineChart.update();
+
+    pieChart.reset();
 }
